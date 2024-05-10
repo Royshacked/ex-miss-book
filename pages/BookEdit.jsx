08c63a3f57c3a4acd17d8b4b60f16dffb2ca664f
Royshacked <roy.shacked@mail.huji.ac.il>
2024-05-10 21:43:16 +0300

    Styling edit book

--- pages/BookEdit.jsx
+++ pages/BookEdit.jsx
@@ -2,6 +2,7 @@ import { bookService } from "../services/books.service.js"
 
 const { useState, useEffect } = React
 const { useParams, useNavigate } = ReactRouter
+const { Link } = ReactRouterDOM
 
 
 export function BookEdit() {
@@ -48,12 +49,20 @@ export function BookEdit() {
 
     return <section className="book-edit">
         <h2>{params.bookId ? 'Edit book' : 'Add book'}</h2>
-
-        <form onSubmit={onSave}>
-            <input onChange={handleChange} type="text" value={book.title} name="title" placeholder="Insert book title..." />
-            <input onChange={handleChange} type="number" value={book.listPrice.amount} placeholder="Insert book tprice..." />
-            <input onChange={handleChange} type="number" value={book.pageCount} placeholder="Insert book page count..." />
-            <button>Save</button>
-        </form>
+        <div className="edit-form">
+            <img src={book.thumbnail} alt="" />
+            <form onSubmit={onSave}>
+                <span>Title:</span><input onChange={handleChange} type="text" value={book.title} name="title" placeholder="Insert book title..." />
+                <span>Authors:</span><input onChange={handleChange} type="text" value={book.authors} name="authors" placeholder="Insert book authors..." />
+                <span>Description:</span><input onChange={handleChange} type="text" value={book.description} name="description" placeholder="Insert book description..." />
+                <span>Categories:</span><input onChange={handleChange} type="text" value={book.categories} name="categories" placeholder="Insert book categories..." />
+                <span>Language:</span><input onChange={handleChange} type="text" value={book.language} name="language" placeholder="Insert book language..." />
+                <span>Price:</span><input onChange={handleChange} type="number" value={book.listPrice.amount} placeholder="Insert book tprice..." />
+                <span>Pages:</span><input onChange={handleChange} type="number" value={book.pageCount} placeholder="Insert book page count..." />
+                <button>Save</button>
+                {/* <img src={book.thumbnail} alt="" /> */}
+            </form>
+        </div>
+        <Link to="/book"><button>X</button></Link>
     </section>
 }
