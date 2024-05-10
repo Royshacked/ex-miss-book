c7d8c2a0ed9d96d65842d6ae512b21cdf16733fd
Royshacked <roy.shacked@mail.huji.ac.il>
2024-05-10 21:55:19 +0300

    Final stylw touch

--- pages/BookEdit.jsx
+++ pages/BookEdit.jsx
@@ -45,12 +45,12 @@ export function BookEdit() {
             })
     }
 
-    console.log(book)
-
     return <section className="book-edit">
         <h2>{params.bookId ? 'Edit book' : 'Add book'}</h2>
+
         <div className="edit-form">
             <img src={book.thumbnail} alt="" />
+
             <form onSubmit={onSave}>
                 <span>Title:</span><input onChange={handleChange} type="text" value={book.title} name="title" placeholder="Insert book title..." />
                 <span>Authors:</span><input onChange={handleChange} type="text" value={book.authors} name="authors" placeholder="Insert book authors..." />
@@ -60,7 +60,6 @@ export function BookEdit() {
                 <span>Price:</span><input onChange={handleChange} type="number" value={book.listPrice.amount} placeholder="Insert book tprice..." />
                 <span>Pages:</span><input onChange={handleChange} type="number" value={book.pageCount} placeholder="Insert book page count..." />
                 <button>Save</button>
-                {/* <img src={book.thumbnail} alt="" /> */}
             </form>
         </div>
         <Link to="/book"><button>X</button></Link>
